25ba53da5d278595a7ad084b19b5e5e716bbfc0b
Maksim Dzjubenko <90320393+SvinDzjuba@users.noreply.github.com>
2023-04-16 00:13:55 +0300

    Made the posts render

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,18 +1,57 @@
 import { CiSearch } from 'react-icons/ci';
+import { FiSend } from 'react-icons/fi';
 
 const Posts = () => {
+    let posts = require('../data/posts.json');
+    let comments = require('../data/comments.json');
+    let data = [];
+    for (let i = 1; (i - 1) < posts.length; i++) {
+        let answers = [];
+        for (let j = 0; j < comments.length; j++) {
+            if(comments[j].postId === posts[i].id) {
+                answers.push(comments[j]);
+            }
+        }
+        let obj = {
+            post: posts[i],
+            comments: answers
+        };
+        data.push(obj);
+
+        if(i === 15) break;
+    }
     return (
         <div className="posts_page">
             <div className="posts_container">
                 <div className="posts_header">
-                    <h1>Posts List</h1>
-                    <input type="text" autoFocus placeholder="Keyword..." />
-                    <button className="btn"><CiSearch /></button>
+                    <h1>User posts from all over the world</h1>
+                    <div>
+                        <input type="text" className="form-control" autoFocus placeholder="Keyword..." />
+                        <button className="btn btn-secondary"><CiSearch /></button>
+                    </div>
                 </div>
-                
+                <div className="posts_list">
+                    {
+                        data.map(data => {
+                            return (
+                                <div className="post">
+                                    <h2>{data.post.title}</h2>
+                                    <p>{data.post.body}</p>
+                                    <div className="bottom">
+                                        <div>
+                                            <input type="text" placeholder='Reply...' />
+                                            <button className='btn'><FiSend /></button>
+                                        </div>
+                                        <button className="btn btn-primary">Answers</button>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             </div>
         </div>
     )
 }
 
-export default Posts
+export default Posts;
